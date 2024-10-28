e151448aa45ae90c1ae1c9e27ae7884226b90107
Sai Nishit Ragupathy <sr6931@nyu.edu>
2024-10-27 21:19:21 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,22 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(255, 255, 0); 
+
+  let gridSize = 100; 
+
+  for (let x = gridSize / 2; x < width; x += gridSize) {
+    for (let y = gridSize / 2; y < height; y += gridSize) {
+      let size = (x + y) % 200 === 0 ? gridSize * 0.8 : gridSize * 0.4; 
+      let offsetX = random(-5, 5); 
+      let offsetY = random(-5, 5); 
+
+      fill(0);
+      ellipse(x + offsetX, y + offsetY, size); /
+    }
+  }
 }
 
-function draw() {
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  redraw(); 
 }
